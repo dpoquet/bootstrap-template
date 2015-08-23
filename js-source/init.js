4e093e768490f58dbebf0b9188b06837f690adf2
dpoquet <Dani@MacBook-Pro-de-Daniel.local>
2015-08-23 23:26:57 +0200

    Bootstrap template with SASS and Gulp
    Bootstrap template with SASS and Gulp v1.0

---
+++ js-source/init.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function() {
+
+	// Declare global vars
+
+
+	// Functions //
+
+
+
+
+
+
+	// Init Functions //
+
+
+	$( window ).resize(function(){
+
+  		// Init functions on resize //
+
+
+	});
+
+
+
+	$( window ).scroll(function(){
+
+  		// Init functions on scroll //
+
+	});
+
+});
+
+
+
+// Global Functions //
